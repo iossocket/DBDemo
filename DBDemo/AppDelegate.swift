d435c353e98a4b76cc041b12ba1fffa2f6fe16b7
XueliangZhu <xlzhu@xlzhu.local>
2016-12-27 09:48:50 +0800

    init project

---
+++ DBDemo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DBDemo
+//
+//  Created by XueliangZhu on 12/19/16.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ DBDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  DBDemo
+//
+//  Created by XueliangZhu on 12/19/16.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
